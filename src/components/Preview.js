99a0a825d64be07b284175c37ffc3ad6d4f9326a
Chaitali996 <59926324+Chaitali996@users.noreply.github.com>
2022-04-15 23:34:08 +0530

    Update Preview.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Preview = ({fontSize,content,padding}) =>{
     return (
-        <div id="preview" style={{padding,fontSize}}>
+        <div id="preview" style={{padding,fontSize,contentInput}}>
             {content}
         </div>
     )
