c50e85ac3566a52563289eae4ec8ec870f3115e0
Chaitali996 <59926324+Chaitali996@users.noreply.github.com>
2022-04-15 23:33:10 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,13 @@ const App = () => {
     <div id="main">
       <div>
         {/* add input elememnts inside here */}
+        <input id ="contentInput">hello world</input>
+        <input type ="number" id="fontSizeInput" value="10"></input>
+        <input type ="number" id="paddingInput" value="5"></input>
       </div>
  
  {/* render Preview component here */}
- 
+      <Preview />
     </div>
   )
 }
